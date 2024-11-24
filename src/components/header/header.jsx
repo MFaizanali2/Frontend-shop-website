cc8a90bcd04ea8d707d121cb242cef9594170acf
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-11-25 02:03:07 +0500

    first commidt

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
 
 const header = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div className='header-wrapper'>
+            <div className="some">
+                    <h6>Sign up and get 20% off on your first order</h6>
+                </div>
+            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                <div class="container-fluid">
+                    <img src="/assets/img/shop.co.png" alt="" class="navbar-brand" />
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">OnSale</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">NewArrivals</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Brands</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Shops
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="#">Action</a></li>
+                                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                    <form class="d-flex header-icon" role="search">
+                        <input class="form-control me-2" btn-outline-success type="search" placeholder="Search" aria-label="Search" />
+                        <i class="fa-solid fa-cart-shopping"></i>
+                        <i class="fa-solid fa-user"></i>
+                    </form>
+                </div>
+            </nav>
+        </div>
+    )
 }
 
 export default header
---
+++ src/pages/home.jsx
@@ -0,0 +1,12 @@
+import Header from "../components/header/header"
+
+
+const home = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
+
+export default home
